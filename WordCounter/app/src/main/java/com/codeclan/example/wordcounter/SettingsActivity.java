7b020954e508049418477c28df627657f2ebe5be
Yoni Satat <yoni.satat@gmail.com>
2017-11-14 21:22:43 +0000

    added AboutActivity and SettingsActivity

---
+++ WordCounter/app/src/main/java/com/codeclan/example/wordcounter/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.codeclan.example.wordcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 14/11/2017.
+ */
+
+class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_about);
+    }
+}
